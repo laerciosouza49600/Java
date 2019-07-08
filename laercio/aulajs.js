424fc5e2e7a04f162d329264cb9c45ab7e2dc03a
Laercio Souza da Silva <20161TIMINF013@ifpi.int>
2019-07-08 14:59:01 -0300

    pw

---
+++ laercio/aulajs.js
@@ -0,0 +1,32 @@
+
+// one line
+
+
+alert('Pagina em JAVASCRIPT'); // alerta inicio
+
+/*
+
+comentario multilinhas */
+
+// document.write("<h2 align = 'center'> Pagina em JS - aula </h2"); centralizado separado
+
+document.write("texto em js na pagina html");
+
+//  nome = prompt("digite seu nome?"); 
+
+//  document.write("<b> seu nome é: </b> " + nome);
+
+var
+
+a = 10;
+b = 2.5;
+c = 10 + 2.5; //    " - / * % ** " operadores lógicos
+
+
+// < > <= >= == != operadores condicionais
+
+// && || operadores lógicos " and or"
+
+
+
+
